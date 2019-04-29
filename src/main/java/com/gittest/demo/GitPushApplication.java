1788c015d446f78ac7518f72171aa58cdceb30fb
Daniel Wang <dannywx18@gmail.com>
2019-04-28 22:56:56 -0400

    test

---
+++ src/main/java/com/gittest/demo/GitPushApplication.java
@@ -0,0 +1,13 @@
+package com.gittest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPushApplication.class, args);
+	}
+
+}
